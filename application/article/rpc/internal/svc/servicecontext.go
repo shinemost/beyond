ae0ecf4753134443ef3fe81e6a131949a7d5528f
shinemost <supertain147@163.com>
2024-03-27 15:28:07 +0800

    article rpc服务配置redis缓存，缓存逻辑待实现

--- application/article/rpc/internal/svc/servicecontext.go
+++ application/article/rpc/internal/svc/servicecontext.go
@@ -3,17 +3,20 @@ package svc
 import (
 	"github.com/shinemost/beyond/application/article/rpc/internal/config"
 	"github.com/shinemost/beyond/application/article/rpc/internal/model"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config       config.Config
 	ArticleModel model.ArticleModel
+	BizRedis     *redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:       c,
 		ArticleModel: model.NewArticleModel(sqlx.NewMysql(c.DataSource), c.CacheRedis),
+		BizRedis:     redis.MustNewRedis(c.BizRedis),
 	}
 }
